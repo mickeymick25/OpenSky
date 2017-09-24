1588bd8631f4e77d4f83196c5d44df2f7107ffd3
mickeymick25 <mickeymick25@gmail.com>
2017-09-24 20:57:21 +0200

    change less to sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
-var less = require('gulp-less');
+// var less = require('gulp-less');
+var sass = require('gulp-sass');
 var minify = require('gulp-minify');
 var browserSync = require('browser-sync').create();
 
 // Tasks
-gulp.task('default', ['pug','less','jsroutes']);
+gulp.task('default', ['pug','sass','jsroutes']);
 
 gulp.task('pug', function(){
  return gulp.src( 'views/**/*.pug')
@@ -13,10 +14,16 @@ gulp.task('pug', function(){
  .pipe( gulp.dest('dist/html/'));
 });
 
-gulp.task('less', function(){
-  return gulp.src( 'public/less/**/*.less')
-  .pipe( less())
-  .pipe(gulp.dest( 'dist/stylesheets/'));
+// gulp.task('less', function(){
+//   return gulp.src( 'public/less/**/*.less')
+//   .pipe( less())
+//   .pipe(gulp.dest( 'dist/stylesheets/'));
+// });
+
+gulp.task('sass', function(){
+  return gulp.src('public/sass/**/*.sass')
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(gulp.dest('dist/stylesheets/'));
 });
 
 gulp.task('jsroutes',function(){
@@ -37,7 +44,9 @@ gulp.task('watch', function(){
   gulp.watch('views/**/*.pug', [ 'pug'])
   .on('change', browserSync.reload);
 
-  gulp.watch('public/less/**/*.less', [ 'less'])
+ //  gulp.watch('public/less/**/*.less', [ 'less'])
+ // .on('change', browserSync.reload);
+  gulp.watch('public/sass/**/*.sass', [ 'sass'])
  .on('change', browserSync.reload);
 
   gulp.watch('routes/**/*.js', [ 'jsroutes'])
